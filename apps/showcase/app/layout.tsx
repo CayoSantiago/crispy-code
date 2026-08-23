949a16be1885354af76bc9d70c61eb7a771bd5b4
Brandon Crisp <brandonc577@gmail.com>
2026-08-23 14:09:09 -0400

    added scaffold for showcase app

---
+++ apps/showcase/app/layout.tsx
@@ -0,0 +1,41 @@
+import '@repo/ui/globals.css'
+import { cn } from '@repo/ui/lib/utils'
+
+import type { Metadata } from 'next'
+import { Geist_Mono, Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/theme-provider'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+
+const fontMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+})
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Crispy Code',
+    default: 'Crispy Code',
+  },
+}
+
+export default function RootLayout({ children }: LayoutProps<'/'>) {
+  return (
+    <html
+      lang='en'
+      suppressHydrationWarning
+      data-scroll-behavior='smooth'
+      className={cn(
+        'antialiased',
+        fontMono.variable,
+        'font-sans',
+        inter.variable,
+        'scroll-smooth',
+      )}
+    >
+      <body suppressHydrationWarning>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ apps/showcase/app/page.tsx
@@ -0,0 +1,3 @@
+export default function ShowCaseHomePage() {
+  return <div>Show Case</div>
+}
